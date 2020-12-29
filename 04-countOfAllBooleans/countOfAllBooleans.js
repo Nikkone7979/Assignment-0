1250f66419b43c8a26ea2ab9bb02bb7e1592d968
Youness Elwiddi <ywiddi1020@gmail.com>
2020-12-29 02:40:51 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,17 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+    var x = 0;
+  for (var i = 0; i < arr.length; i++){
+      if (arr[i] === true){
+          x++;
+      } 
+        if (arr[i] === false){
+          x++;
+      } 
+      
+  }
+  return x;
 }
 
+countOfAllBooleans(["hello", "world", 3, "nice", 99, Infinity, -Infinity, true, true, true, false]);
+
 // Do not edit this line;
-module.exports = countOfAllBooleans;
